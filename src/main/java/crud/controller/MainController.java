78ef62d928df2e48ce8958cd5653088ba3d6d6fd
Admin <mkuzivanov@gmail.com>
2020-05-07 21:46:11 +0300

    3.1.2

--- src/main/java/crud/controller/MainController.java
+++ src/main/java/crud/controller/MainController.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
 @Controller
 @RequestMapping("/main")
 @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER')")
